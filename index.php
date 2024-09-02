3bdadf6b1b04ce09c24d0c2bdc76e19d72383c77
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-09-02 08:51:38 +0700

    feat: index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+
+    <body>
+        <?php
+        for($x = 1; $x <= 100; $x++){
+            echo "<button> button ke-$x</button>";
+            echo "<br/>";
+        }
+        ?>
+    </body>
+</html>
