17eee8d200744b9e267a2f31bbce9ce828c8514c
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-09-06 17:56:41 +0700

    feat: new file

---
+++ lk2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1><?php
+
+        $hello = "Hello";
+        $world = "World!";
+        echo $hello, $world;
+
+        ?></h1>
+
+    </body>
+</html>
