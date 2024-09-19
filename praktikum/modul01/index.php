77cf690fac8f62d830922f04a3515ed77a3c8227
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2024-09-19 07:30:05 +0700

    feat: add new file

---
+++ praktikum/modul01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo "Praktikum Modul 01" ?></title>
+    </head>
+
+    <?php
+        $name  = "Elgin Brian Wahyu Bramadhika";
+        $nim   = 235150701111031;
+        $email = "elginbrian49@student.ub.ac.id";
+    ?>
+
+    <body>
+         <?php
+            echo "<h1>Data Diri</h1>";
+            echo "<hr/>";
+            echo "<p>Nama: $name</p>";
+            echo "<p>NIM: $nim</p>";
+            echo "<p>Email: $email</p>";
+        ?>
+    </body>
+</html>
---
+++ praktikum/modul02/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo "Praktikum Modul 02" ?></title>
+    </head>
+
+    <body>
+        
+    </body>
+</html>
